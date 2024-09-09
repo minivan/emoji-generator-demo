8302b39570d1685c7db2db1190eaf738ca1cd50f
Ivan Zarea <ivan.zarea@netlify.com>
2024-09-09 19:32:00 +0200

    Changed styles

--- netlify/functions/generate-emoji.ts
+++ netlify/functions/generate-emoji.ts
@@ -7,9 +7,22 @@ const replicate = new Replicate({
 export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
   const emojiText = url.searchParams.get("query");
+
   const input = {
-    prompt: `A ${emojiText} emoji`,
+    mask: "https://replicate.delivery/pbxt/JT6K0z55b9kVVAvjzirEYBSMB3kGbRWdw6nbdxsvbIqY4K99/1024x1024-border-mask.png",
+    width: 1024,
+    height: 1024,
+    prompt: `A TOK emoji of ${emojiText}, white background`,
+    refine: "expert_ensemble_refiner",
+    scheduler: "K_EULER",
+    lora_scale: 0.6,
+    num_outputs: 1,
+    guidance_scale: 10.55,
     apply_watermark: false,
+    high_noise_frac: 1,
+    negative_prompt: "soft, underexposed, cropped",
+    prompt_strength: 1,
+    num_inference_steps: 50,
   };
 
   const image_url = await replicate.run(
@@ -20,7 +33,7 @@ export default async (request: Request, context: Context) => {
     { image_url },
     {
       headers: {
-        "Netlify-CDN-Cache-Control": "public, max-age=86400, durable",
+        "Netlify-CDN-Cache-Control": "public, max-age=600, durable",
         "Netlify-Vary": "query",
       },
     },
